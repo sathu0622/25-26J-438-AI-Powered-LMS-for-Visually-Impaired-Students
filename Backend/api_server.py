f37d4cf678f91cdfc8c79f6517cf715ec340070a
sathu0622 <sathushan622@gmail.com>
2026-03-09 08:48:58 +0530

    fix cros error

--- Backend/api_server.py
+++ Backend/api_server.py
@@ -41,7 +41,7 @@ app = FastAPI(title="Article Detection Document Processor with Q&A", version="6.
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
